a19a0d1da57571d11404b669904fe779a192cbe1
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-16 17:13:15 -0800

    adjusted test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ListItemForm from './ListItemForm';
 
-test('renders logout link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/logout/i);
+test('renders Quantity link', () => {
+  render(<ListItemForm />);
+  const linkElement = screen.getByText(/Quantity/i);
   expect(linkElement).toBeInTheDocument();
 });
